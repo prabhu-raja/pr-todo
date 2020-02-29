728b9f533f81dc6d62164359675bfcf1fed46386
prabhu-raja <prabhuraja@gmail.com>
2020-02-29 11:51:19 -0500

    Set default home page as todo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoComponent } from 'src/app/todo/todo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
