3711722f16192a8fd37506d28bcc9fa866d03e1b
prabhu-raja <prabhuraja@gmail.com>
2020-03-02 21:35:31 -0500

    Basic List Itwem display

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoList } from 'src/app/todo/model/todolist.model';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
-
+  todoList$: Observable<ToDoList[]>;
   constructor() { }
 
   ngOnInit() {
+    this.todoList$ = this.getToDoList();
   }
 
+  private getToDoList(): Observable<ToDoList[]> {
+    const allItems = [
+      {
+        id: 101,
+        name: 'Groceries 🛒',
+        isCompleted: false
+      },
+      {
+        id: 102,
+        name: 'Swimming Class Enquiry🏊‍♂️',
+        isCompleted: false
+      },
+      {
+        id: 103,
+        name: 'Pay Credit Card bills 💳',
+        isCompleted: false
+      },
+      {
+        id: 104,
+        name: 'Music class 🎸',
+        isCompleted: false
+      },
+      {
+        id: 105,
+        name: 'Pay Mortgage 💵',
+        isCompleted: false
+      },
+      {
+        id: 106,
+        name: 'Car Wash 🚗',
+        isCompleted: false
+      }
+    ];
+    return of(allItems);
+  }
 }
